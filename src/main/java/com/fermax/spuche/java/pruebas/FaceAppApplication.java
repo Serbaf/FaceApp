05eaf4cbc271871e088130e3f9f552844de06505
ID - Sergio Puche <spuche@fermax.lan>
2018-06-15 14:58:50 +0200

    Añadido un puñao de funcionalidad

--- src/main/java/com/fermax/spuche/java/pruebas/FaceAppApplication.java
+++ src/main/java/com/fermax/spuche/java/pruebas/FaceAppApplication.java
@@ -11,79 +11,225 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FaceAppApplication implements CommandLineRunner {
 
-    // Replace XXXXXXXXXXX with your valid subscription key.
-    private static final String subscriptionKey = "XXXXXXXXXXXX";
+	// Replace XXXXXXXXXXX with your valid subscription key.
+	private static final String subscriptionKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-    // Set here the URL belonging to the image to analyze
-    private static final String imageWithFaces =
-    			"{\"url\":\"https://images.csmonitor.com/csmarchives/2011/10/1007_Wozniak.jpg?alias=standard_600x400\"}";
-    
-    /*
-     * Alternative images:
-     * Bill Gates: "https://upload.wikimedia.org/wikipedia/commons/0/01/Bill_Gates_July_2014.jpg"
-     * Luise Lillian Gish: "https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg"
-     */
-    
-    // Facial attributes to analyze
-    private static final String faceAttributes =
-        "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";	
-	
-    
-    
-    /*
-     * METHODS
-     */
-    
-    // Spring substitute of the main method
-	public void run(String ... args)
-	{
+	// Set here the URL belonging to the image to analyze
+	private static final String imageWithFaces = "{\"url\":\"https://images.csmonitor.com/csmarchives/2011/10/1007_Wozniak.jpg?alias=standard_600x400\"}";
+
+	/*
+	 * Alternative images: Bill Gates:
+	 * "https://upload.wikimedia.org/wikipedia/commons/0/01/Bill_Gates_July_2014.jpg"
+	 * Luise Lillian Gish:
+	 * "https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg"
+	 */
+
+	// Facial attributes to analyze
+	private static final String faceAttributes = "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+	private Scanner keyboard;
+
+	/*
+	 * METHODS
+	 */
+
+	// Spring substitute of the main method
+	public void run(String... args) {
+
 		// Scanner de teclado. En el futuro se puede sustituir por algo mejor
-		Scanner keyboard = new Scanner(System.in);
-		
-        try
-        {
-        	System.out.println("********************************************************");
-        	System.out.println("*  APLICACION DE DETECCION FACIAL CON MOTOR FACE API   *");
-        	System.out.println("********************************************************");
-        	System.out.println();
-        	System.out.println();
-        	System.out.println();
-        	System.out.println("Modos de empleo:");
-        	System.out.println("1. Deteccion facial");
-        	
-        	System.out.println();
-        	System.out.println("Introduzca la opcion deseada: ");
-        	int opcion = keyboard.nextInt();
-        	
-        	switch (opcion)
-        	{
-        		case 1:		FaceMethods.faceDetect(imageWithFaces, faceAttributes, subscriptionKey);
-        					break;
-        				
-        		case 2:		FaceList facesList = new FaceList("listaPrueba", subscriptionKey);
-        					break;
-        				
-        		default:	System.out.println("Opcion invalida. Autodestruccion en 5 segundos");
-        					break;
-        	}
-        	
-        	keyboard.close();
+		keyboard = new Scanner(System.in);
 
-        }
-        
-        catch (Exception e)
-        {
-            // Display error message.
-            System.out.println(e.getMessage());
-        }
+		try {
+
+			System.out.println("********************************************************");
+			System.out.println("*  APLICACION DE DETECCION FACIAL CON MOTOR FACE API   *");
+			System.out.println("********************************************************");
+			System.out.println();
+
+			while (true) {
+				mainMenu();
+			}
+
+		}
+
+		catch (Exception e) {
+			// Display error message.
+			System.out.println(e.getMessage());
+		}
 	}
-	
-	
-	
+
+	/**
+	 * Main menu of the application Allows the navigation to the submenus with
+	 * actual functionality Also allows the termination of the program
+	 * 
+	 * @throws Exception
+	 */
+	private void mainMenu() throws Exception {
+		System.out.println();
+		System.out.println();
+		System.out.println("Modos de empleo:");
+		System.out.println("1. Face");
+		System.out.println("2. FaceList");
+		System.out.println("0. Quit the program");
+
+		System.out.println();
+		System.out.println("Introduzca la opcion deseada: ");
+		int opcion = keyboard.nextInt();
+
+		switch (opcion) {
+		case 1:
+			menuFace();
+			break;
+
+		case 2:
+			menuFaceList();
+			break;
+
+		case 0:
+			System.exit(0);
+
+		default:
+			System.out.println("Opcion invalida. Autodestruccion en 5 segundos");
+			break;
+		}
+	}
+
+	/**
+	 * Menu for the "Face" functions
+	 * 
+	 * @throws Exception
+	 */
+	private void menuFace() throws Exception {
+		while (true) {
+			System.out.println();
+			System.out.println();
+			System.out.println(" Opciones menú Face");
+			System.out.println("**********************");
+			System.out.println();
+			System.out.println("1. Detect face");
+			System.out.println("2. Find similar **not implemented**");
+			System.out.println("3. Group **not implemented**");
+			System.out.println("4. Identify **not implemented**");
+			System.out.println("5. Verify **not implemented**");
+			System.out.println("0. Quit");
+
+			System.out.println();
+			System.out.println("Introduzca la opcion deseada: ");
+			int opcion = keyboard.nextInt();
+
+			switch (opcion) {
+			case 1:
+				FaceMethods.faceDetect(imageWithFaces, faceAttributes, subscriptionKey);
+				break;
+
+			case 2:
+				System.out.println("Find similar - Not implemented yet");
+				break;
+
+			case 3:
+				System.out.println("Group - Not implemented yet");
+				break;
+
+			case 4:
+				System.out.println("Identify - Not implemented yet");
+				break;
+
+			case 5:
+				System.out.println("Verify - Not implemented yet");
+				break;
+
+			case 0:
+				return;
+
+			default:
+				System.out.println("Opcion invalida. Autodestruccion en 5 segundos");
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Menu for the "FaceList" functions
+	 * 
+	 * @throws Exception
+	 */
+	private void menuFaceList() throws Exception {
+
+		while (true) {
+			System.out.println();
+			System.out.println();
+			System.out.println(" FaceList menu options");
+			System.out.println("***********************");
+			System.out.println();
+			System.out.println("1. Create face list");
+			System.out.println("2. Add face to a list");
+			System.out.println("3. Delete list");
+			System.out.println("4. Delete face");
+			System.out.println("5. Get info from a list");
+			System.out.println("6. List all the lists");
+			System.out.println("7. Update **not implemented**");
+			System.out.println("0. Quit");
+
+			System.out.println();
+			System.out.println("Introduzca la opcion deseada: ");
+			int opcion = keyboard.nextInt();
+
+			String input1, input2;
+
+			switch (opcion) {
+			case 1:
+				FaceList faceList = new FaceList("facelist1", subscriptionKey);
+				break;
+
+			case 2:
+				System.out.println("Please input the ID of the list: ");
+				input1 = keyboard.next();
+				System.out.println("Please input the URL of the picture: ");
+				input2 = keyboard.next();
+				FaceList.addImage(input1, input2, subscriptionKey);
+				break;
+
+			case 3:
+				System.out.println("Please input the ID of the list you want to delete: ");
+				input1 = keyboard.next();
+				FaceList.deleteList(input1, subscriptionKey);
+				break;
+
+			case 4:
+				System.out.println("Please input the ID of the list from which you want to delete: ");
+				input1 = keyboard.next();
+				System.out.println("Please input the ID of the face you want to delete: ");
+				input2 = keyboard.next();
+				FaceList.deleteFace(input1, input2, subscriptionKey);
+				break;
+
+			case 5:
+				System.out.println("Enter the id of a list:");
+				input1 = keyboard.next();
+				FaceList.getListInfo(input1, subscriptionKey);
+				break;
+
+			case 6:
+				FaceList.listLists(subscriptionKey);
+				break;
+
+			case 7:
+				System.out.println("Update - Not implemented yet");
+				break;
+
+			case 0:
+				return;
+
+			default:
+				System.out.println("Opcion invalida. Autodestruccion en 5 segundos");
+				break;
+			}
+		}
+	}
+
 	// Main method. It just calls to Spring Boot
 	public static void main(String[] args) {
 		SpringApplication.run(FaceAppApplication.class, args);
 	}
-	
-	
+
 }
--- src/main/java/com/fermax/spuche/java/pruebas/FaceList.java
+++ src/main/java/com/fermax/spuche/java/pruebas/FaceList.java
@@ -5,39 +5,206 @@ import java.net.URI;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPatch;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 public class FaceList {
-	
+
 	private String id;
-	
-	public FaceList(String id, String key) throws Exception
-	{
+	private static HttpClient httpClient = HttpClients.createDefault();
+
+	/**
+	 * CONSTRUCTOR
+	 * 
+	 * Create a new List It will be empty in the first place, and the user will have
+	 * to add images
+	 * 
+	 * @param id
+	 *            de la nueva lista
+	 * @param key
+	 *            clave de suscripcion al servicio de Azure
+	 * @throws Exception
+	 */
+	public FaceList(String id, String key) throws Exception {
 		this.id = id;
-		HttpClient httpClient = HttpClients.createDefault();
-		
-		URIBuilder builder = new URIBuilder("https://westus.api.cognitive.microsoft.com/face/v1.0/facelists/id");
+
+		URIBuilder builder = new URIBuilder(
+				"https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/" + id);
+
+		// Create an HTTP PUT request
+		URI uri = builder.build();
+		HttpPut request = new HttpPut(uri);
+
+		// Set the header to JSON
+		request.setHeader("Content-Type", "application/json");
+		request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+		// Create body of the request
+		String name = "lista";
+		String userData = "Imagenes de imbeciles";
+		String body = "{\"name\":\"" + id + "\",\"userData\":\"" + userData + "\"}";
+
+		// Request body
+		StringEntity reqEntity = new StringEntity(body);
+		request.setEntity(reqEntity);
+
+		HttpResponse response = httpClient.execute(request);
+		getResults(response);
+	}
+
+	/**
+	 * List all the image lists stored
+	 * 
+	 * @param key
+	 *            for Azure access
+	 * @throws Exception
+	 */
+	public static void listLists(String key) throws Exception {
+		URIBuilder builder = new URIBuilder("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists");
+
+		URI uri = builder.build();
+		HttpGet request = new HttpGet(uri);
+		request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+		HttpResponse response = httpClient.execute(request);
+		getResults(response);
+	}
+
+	/**
+	 * 
+	 * @param faceListId
+	 *            identifier of the wanted list
+	 * @param key
+	 *            for Azure access
+	 * @throws Exception
+	 */
+	public static void getListInfo(String faceListId, String key) throws Exception {
+		URIBuilder builder = new URIBuilder(
+				"https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/" + faceListId);
+
+		URI uri = builder.build();
+		HttpGet request = new HttpGet(uri);
+		request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+		HttpResponse response = httpClient.execute(request);
+		getResults(response);
+	}
+
+	/**
+	 * Add a face image to a previously constructed list of images
+	 * 
+	 * @param faceListId
+	 *            list identifier
+	 * @param picURL
+	 *            URL of the pic to be added
+	 * @param key
+	 *            access to Azure
+	 * @throws Exception
+	 */
+	public static void addImage(String faceListId, String picURL, String key /* , String userData, String targetFace */)
+			throws Exception {
+
+		URIBuilder builder = new URIBuilder("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/"
+				+ faceListId + "/persistedFaces");
+
+		// builder.setParameter("userData", "{string}");
+		// builder.setParameter("targetFace", "{string}");
+
+		URI uri = builder.build();
+		HttpPost request = new HttpPost(uri);
+		request.setHeader("Content-Type", "application/json");
+		request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+		// Generate a request body
+		// Create body of the request
+		String body = "{\"url\":\"" + picURL + "\"}";
+
+		// Request body
+		StringEntity reqEntity = new StringEntity(body);
+		request.setEntity(reqEntity);
+
+		HttpResponse response = httpClient.execute(request);
+		getResults(response);
+
+	}
+
+	/**
+	 * Given the ID of a list, this method deletes the list from the registry
+	 * 
+	 * @param faceListId
+	 *            identifier of the list
+	 * @param key
+	 *            access to Azure
+	 * @throws Exception
+	 */
+	public static void deleteList(String faceListId, String key) throws Exception {
+		URIBuilder builder = new URIBuilder(
+				"https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/" + faceListId);
+
+		URI uri = builder.build();
+		HttpDelete request = new HttpDelete(uri);
+		request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+		HttpResponse response = httpClient.execute(request);
+		getResults(response);
+
+	}
+
+	/**
+	 * 
+	 * @param faceListId
+	 *            identifier of the list
+	 * @param picId
+	 *            identifier of the picture to be deleted
+	 * @param key
+	 *            access to Azure
+	 * @throws Exception
+	 */
+	public static void deleteFace(String faceListId, String picId, String key) throws Exception {
+		URIBuilder builder = new URIBuilder("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/"
+				+ faceListId + "/persistedFaces/" + picId);
+
+		URI uri = builder.build();
+		HttpDelete request = new HttpDelete(uri);
+		request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+		HttpResponse response = httpClient.execute(request);
+		getResults(response);
+	}
+	
+	public static void updateList(String faceListId, String key) throws Exception
+	{
+		URIBuilder builder = new URIBuilder("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/facelists/"+faceListId);
 		
 		URI uri = builder.build();
-        HttpPut request = new HttpPut(uri);
+        HttpPatch request = new HttpPatch(uri);
         request.setHeader("Content-Type", "application/json");
         request.setHeader("Ocp-Apim-Subscription-Key", key);
-        
-        // Request body
-        StringEntity reqEntity = new StringEntity("{body}");
-        request.setEntity(reqEntity);
+	}
 
-        HttpResponse response = httpClient.execute(request);
-        HttpEntity entity = response.getEntity();
-		
-        if (entity != null) 
-        {
-            System.out.println(EntityUtils.toString(entity));
-        }
+	/**
+	 * Just an internal method to summarize some commonly repeated code lines when
+	 * getting the HTTP response and displaying the results
+	 * 
+	 * @param response
+	 *            HTTP response
+	 * @throws Exception
+	 */
+	//POR CONSTRUIR
+	private static void getResults(HttpResponse response) throws Exception {
+		HttpEntity entity = response.getEntity();
+
+		if (entity != null) {
+			System.out.println(EntityUtils.toString(entity));
+		}
 	}
 
 }
